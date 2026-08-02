c064faf9146a9a3b7a175376a1b5dbaa3629e47c
riktez8-hue <riktez8-hue@users.noreply.github.com>
2026-08-02 19:19:59 +0000

    Initial commit

---
+++ settings.js
@@ -0,0 +1,12 @@
+module.exports = {
+    startimage: 'https://files.catbox.moe/uvh9dt.png',
+    ownerNumber: process.env.OWNER_NUMBER || '92302742882',
+    botName: 'ZESHOO MINI BOT',
+    ownerName: 'ZESHOO',
+    whatsappChannel: 'https://whatsapp.com/channel/0029Vb8vvB1Fcow4AY0NeC1p',
+    tgOwnerId: process.env.OWNER_TELEGRAM_ID || '8488081516',
+    premiumUsers: [],
+    connectedBots: [],
+    version: '3.0.0',
+    prefix: '.'
+};
