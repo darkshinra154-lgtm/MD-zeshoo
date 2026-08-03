ea46c42df60c680524ecf1234ad5ebb9aff039f7
riktez8-hue <riktez8@gmail.com>
2026-08-03 13:58:16 +0530

    settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ module.exports = {
     botName: 'ZESHOO MINI BOT',
     ownerName: 'ZESHOO',
     whatsappChannel: 'https://whatsapp.com/channel/0029Vb8vvB1Fcow4AY0NeC1p',
-    tgOwnerId: process.env.OWNER_TELEGRAM_ID || '8488081516',
+    tgOwnerId: process.env.OWNER_TELEGRAM_ID || '947152915',
     premiumUsers: [],
     connectedBots: [],
     version: '3.0.0',
