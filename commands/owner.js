7ed7ea54e0e69da93a336ef97a310ff50c63ae78
riktez8-hue <riktez8@gmail.com>
2026-08-06 03:58:09 +0530

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -4,7 +4,7 @@ async function ownerCommand(sock, from, msg) {
     const ownerText = `👤 *BOT OWNER:* ${settings.ownerName}\n` +
                     `📱 *NUMBER:* +${settings.ownerNumber}\n` +
                     `🔗 *OFFICIAL WHATSAPP CHANNEL:*\n` +
-                    `> *https://whatsapp.com/channel/0029Vb8RIvDHVvTgHqEiRY1N*`;
+                    `> *Follow the MD-ZESHOO BOT channel on WhatsApp: https://whatsapp.com/channel/0029Vb8vvB1Fcow4AY0NeC1p*`;
     await sock.sendMessage(from, { text: ownerText }, { quoted: msg });
 }
 
