5e93183aaad22d2c3940e3a14a4e52032b4a87c7
riktez8-hue <riktez8@gmail.com>
2026-08-06 12:31:11 +0530

    Update private.js

--- commands/private.js
+++ commands/private.js
@@ -2,7 +2,7 @@ async function privateCommand(sock, from, msg, isAdmin, session) {
     if (!isAdmin) return await sock.sendMessage(from, { text: "❌ Only owner can use this command." }, { quoted: msg });
     
     session.isPublic = false;
-    await sock.sendMessage(from, { text: "🔐 Bot is now in PRIVATE mode. Only the owner can use it." }, { quoted: msg });
+    await sock.sendMessage(from, { text: "🔐 Bot is now in PRIVATE mode. Only the owner use karsakte hein." }, { quoted: msg });
 }
 
 module.exports = privateCommand;
