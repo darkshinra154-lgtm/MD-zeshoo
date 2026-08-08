fdf85b492e43e96447efbc3a09c4d5dc6982d98a
riktez8-hue <riktez8@gmail.com>
2026-08-08 14:58:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 module.exports = {
     startimage: 'https://files.catbox.moe/qwvzbn.png',
-    ownerNumber: process.env.OWNER_NUMBER || '918850852854',
+    ownerNumber: process.env.OWNER_NUMBER || '92302742882',
     botName: 'ZESHOO MINI BOT',
     ownerName: 'ZESHOO',
     whatsappChannel: 'https://whatsapp.com/channel/0029Vb8vvB1Fcow4AY0NeC1p',
     tgOwnerId: process.env.OWNER_TELEGRAM_ID || '947152915',
     premiumUsers: [],
     connectedBots: [],
-    version: '4.0.0',
+    version: '3.0.0',
     prefix: '.'
 };
